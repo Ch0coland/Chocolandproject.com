007dd3271da751152945d8f8bf3f47452c8ff74c
Ch0coland <102547173+Ch0coland@users.noreply.github.com>
2022-04-11 18:03:32 +0300

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -11,7 +11,7 @@ $(function () {
 
 	setTimeout(function () {
 		$('.loader_bg').fadeToggle();
-	}, 0);
+	}, 1500);
 
 	/* Tooltip
 	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
